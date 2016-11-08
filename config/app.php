b6bc6f7a901f1abd4d53938d7a2eaab30f6c4af9
bilel99 <bilel.bekkouche@hotmail.fr>
2016-11-08 20:39:58 +0100

    correction html service provider par collective html service provider new version for laravel 5.3

--- config/app.php
+++ config/app.php
@@ -162,7 +162,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Illuminate\Html\HtmlServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
 
         /*
@@ -229,8 +229,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Form'      => Illuminate\Html\FormFacade::class,
-        'HTML'      => Illuminate\Html\HtmlFacade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+
 
         // Nav Bar debug
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
