b51692ce5c76313c0a139385a803c053e3337069
bilel99 <bilel.bekkouche@hotmail.fr>
2016-11-24 23:38:55 +0100

    Correction basket

--- resources/views/front/basket/index.blade.php
+++ resources/views/front/basket/index.blade.php
@@ -9,74 +9,80 @@
                     <h3 class="panel-title text-center">Votre panier</h3>
                 </div>
                 <div class="panel-body">
-                    <div class="col-md-12 table-responsive cart_info">
+                    <div class="col-md-12 table-responsive cart_info1">
                         <table class="table table-condensed">
                             <thead>
-                                <tr class="cart_menu">
-                                    <td class="image">Produit</td>
-                                    <td class="description">Description</td>
-                                    <td class="">Prix</td>
-                                    <td class="quantity">Quantity</td>
-                                    <td class="total">Total</td>
-                                    <td></td>
-                                </tr>
+                            <tr class="cart_menu">
+                                <td class="image">Produit</td>
+                                <td class="description">Description</td>
+                                <td class="">Prix</td>
+                                <td class="quantity">Quantity</td>
+                                <td class="total">Total</td>
+                                <td><span><i class="fa fa-trash-o fa-lg"></i></span></td>
+                            </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td class="cart_product">
-                                        <a href=""><img src="http://bycorsica.fr/324-tm_small_default/tapenade-noire-olives-noires-apero.jpg" alt=""></a>
-                                    </td>
-                                    <td class="cart_description">
-                                        <h4><a href="">TAPENADE NOIRE</a></h4>
-                                        <p class="refPr">Référence : BC_AP_0002</p>
-                                    </td>
-                                    <td class="cart_price">
-                                        <p>$59</p>
-                                    </td>
-                                    <td class="cart_quantity">
-                                        <div class="cart_quantity_button">
-                                            <a class="cart_quantity_down"> - </a>
-                                            <input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
-                                            <a class="cart_quantity_up"> + </a>
-                                        </div>
-                                    </td>
-                                    <td class="cart_total">
-                                        <p>$59</p>
-                                    </td>
-                                    <td class="cart_delete">
-                                        <a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="cart_product">
-                                        <a href=""><img src="http://bycorsica.fr/324-tm_small_default/tapenade-noire-olives-noires-apero.jpg" alt=""></a>
-                                    </td>
-                                    <td class="cart_description">
-                                        <h4><a href="">TAPENADE NOIRE</a></h4>
-                                        <p>Référence : BC_AP_0002</p>
-                                    </td>
-                                    <td class="cart_price">
-                                        <p>$59</p>
-                                    </td>
-                                    <td class="cart_quantity">
-                                        <div class="cart_quantity_button">
-                                            <a class="cart_quantity_down"> - </a>
-                                            <input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
-                                            <a class="cart_quantity_up"> + </a>
-                                        </div>
-                                    </td>
-                                    <td class="cart_total">
-                                        <p class="cart_total_price">$59</p>
-                                    </td>
-                                    <td class="cart_delete">
-                                        <a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
-                                    </td>
-                                </tr>
+                            <tr>
+                                <td class="cart_product">
+                                    <a href=""><img
+                                                src="http://bycorsica.fr/324-tm_small_default/tapenade-noire-olives-noires-apero.jpg"
+                                                alt=""></a>
+                                </td>
+                                <td class="cart_description">
+                                    <h4><a href="">TAPENADE NOIRE</a></h4><div class="break"></div>
+                                    <p class="refPr">Référence : BC_AP_0002</p>
+                                </td>
+                                <td class="cart_price">
+                                    <p>$59</p>
+                                </td>
+                                <td class="cart_quantity">
+                                    <div class="cart_quantity_button">
+                                        <a class="cart_quantity_down"> - </a>
+                                        <input class="cart_quantity_input" type="text" name="quantity" value="1"
+                                               autocomplete="off" size="2">
+                                        <a class="cart_quantity_up"> + </a>
+                                    </div>
+                                </td>
+                                <td class="cart_total">
+                                    <p>$59</p>
+                                </td>
+                                <td class="cart_delete">
+                                    <a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="cart_product">
+                                    <a href=""><img
+                                                src="http://bycorsica.fr/324-tm_small_default/tapenade-noire-olives-noires-apero.jpg"
+                                                alt=""></a>
+                                </td>
+                                <td class="cart_description">
+                                    <h4><a href="">TAPENADE NOIRE</a></h4><div class="break"></div>
+                                    <p class="refPr">Référence : BC_AP_0002</p>
+                                </td>
+                                <td class="cart_price">
+                                    <p>$59</p>
+                                </td>
+                                <td class="cart_quantity">
+                                    <div class="cart_quantity_button">
+                                        <a class="cart_quantity_down"> - </a>
+                                        <input class="cart_quantity_input" type="text" name="quantity" value="1"
+                                               autocomplete="off" size="2">
+                                        <a class="cart_quantity_up"> + </a>
+                                    </div>
+                                </td>
+                                <td class="cart_total">
+                                    <p>$59</p>
+                                </td>
+                                <td class="cart_delete">
+                                    <a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
+                                </td>
+                            </tr>
                             </tbody>
                         </table>
                     </div>
 
-                    <div class="code_promo col-md-6">
+                    <div class="code_promo col-md-6 col-sm-12">
                         <form>
                             <div class="form-group col-md-12 col-sm-12">
                                 <label for="secondname">Avez-vous un code promo ? </label>
@@ -93,28 +99,28 @@
                         </form>
                     </div>
 
-                    <div class="col-md-6">
-                        <div class="table-responsive cart_info">
+                    <div class="col-md-6 col-sm-12">
+                        <div class="table-responsive cart_info2">
                             <table class="table table-condensed">
                                 <thead>
-                                    <tr class="cart_menu">
-                                        <td class="image">Total produit TTC</td>
-                                        <td class="description">Frait de port</td>
-                                        <td class="">Total TTC</td>
-                                    </tr>
+                                <tr class="cart_menu">
+                                    <td class="description">Total produit TTC</td>
+                                    <td class="description">Frait de port</td>
+                                    <td class="description">Total TTC</td>
+                                </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td class="cart_product">
-                                            <p>4,43$</p>
-                                        </td>
-                                        <td class="cart_price">
-                                            <p>8,98$</p>
-                                        </td>
-                                        <td class="cart_total">
-                                            <p>13,41$</p>
-                                        </td>
-                                    </tr>
+                                <tr>
+                                    <td class="cart_product">
+                                        <p>4,43$</p>
+                                    </td>
+                                    <td class="cart_price">
+                                        <p>8,98$</p>
+                                    </td>
+                                    <td class="cart_total">
+                                        <p>13,41$</p>
+                                    </td>
+                                </tr>
                                 </tbody>
                             </table>
                         </div>
@@ -122,7 +128,8 @@
 
                     <div class="col-md-12 col-sm-12">
                         <div class="form-group col-md-6 col-sm-6 col-xs-12">
-                            <input type="submit" class="btn btn-primary btn-block pull-right" value="Continuer mes Achats"/>
+                            <input type="submit" class="btn btn-primary btn-block pull-right"
+                                   value="Continuer mes Achats"/>
                         </div>
                         <div class="form-group col-md-6 col-sm-6 col-xs-12">
                             <input type="submit" class="btn btn-primary btn-block" value="Commander"/>
