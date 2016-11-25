44bfa0538ac76494d37a06f64e1ef4dfed69161e
tpenaruiz <tpenaruiz@gmail.com>
2016-11-25 23:07:17 +0100

    Update Commade Phase 4

--- resources/views/front/commande/stepFour.blade.php
+++ resources/views/front/commande/stepFour.blade.php
@@ -35,7 +35,78 @@
         </div>
 
         <div class="row commandeStepFour-content">
-            
+            <div class="col-md-12">
+                <p class="libelle">Selectionnez un mode de paiement</p>
+            </div>
+            <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+                <div class="table-responsive cart_info2">
+                    <table class="table table-condensed">
+                        <thead>
+                        <tr class="cart_menu">
+                            <td class="description">Total produit TTC</td>
+                            <td class="description">Frait de port</td>
+                            <td class="description">Total TTC</td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td class="cart_product">
+                                <p>4,43$</p>
+                            </td>
+                            <td class="cart_price">
+                                <p>8,98$</p>
+                            </td>
+                            <td class="cart_total">
+                                <p>13,41$</p>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>            
+
+            {!! Form::open(['method' => 'post', 'url' => route('home')]) !!}
+            <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+                <div class="row mode-paiement">
+                    <div class="col-md-6 col-sm-6">
+                        <div class="panel panel-default">
+                            <div class="panel-body" style="">
+                                <span class="check">{!! Form::radio('mode-paiement', '0', true) !!}</span>
+                                <span class="image"><img class="responsive" src="http://bycorsica.fr/modules/etransactions/img/CB.png" alt="" title=""></span>
+                                <span class="cb"><label>Payer par Carte Bleue</label></span>
+                            </div>
+                        </div>
+                        <div class="panel panel-default">
+                            <div class="panel-body" style="">
+                                <span class="check">{!! Form::radio('mode-paiement', '0', true) !!}</span>
+                                <span class="image"><img class="responsive" src="http://bycorsica.fr/modules/etransactions/img/CB.png" alt="" title=""></span>
+                                <span class="cb"><label>Payer par Carte Bleue</label></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-6">
+                        <div class="panel panel-default">
+                            <div class="panel-body" style="">
+                                <span class="check">{!! Form::radio('mode-paiement', '0', true) !!}</span>
+                                <span class="image"><img class="responsive" src="http://bycorsica.fr/modules/etransactions/img/CB.png" alt="" title=""></span>
+                                <span class="cb"><label>Payer par Carte Bleue</label></span>
+                            </div>
+                        </div>
+                        <div class="panel panel-default">
+                            <div class="panel-body" style="">
+                                <span class="check">{!! Form::radio('mode-paiement', '0', true) !!}</span>
+                                <span class="image"><img class="responsive" src="http://bycorsica.fr/modules/etransactions/img/CB.png" alt="" title=""></span>
+                                <span class="cb"><label>Payer par Carte Bleue</label></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+                {!! Form::submit('Regler votre commande', ['class' => 'btn btn-primary btn-block']) !!}
+            </div>
+            {!! Form::close() !!}
         </div>
     </div>
 @stop
