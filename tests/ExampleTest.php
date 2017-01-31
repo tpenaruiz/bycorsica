7363ac176cf1be14be54c2d198f378b43d71ba41
bilel99 <bilel.bekkouche@hotmail.fr>
2017-01-31 14:34:22 +0100

    test unit + optimization ajax

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,7 +13,16 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel');
+        /*$this->visit('/')
+             ->see('Laravel');*/
+
+        // Grand 1: On initialise les données exemple :
+        $data = [10, 20, 30];
+        // Grand 2: On agit sur ces données exemple :
+        $result = array_sum($data);
+        // Grand 3: On teste le resultat exemple :
+        $this->assertEquals(60, $result);
+        // Grand 4: On teste si tous ce passe bien avec la commande php phpunit.phar,
+        // bien sur le but est que tous passe vert :-)
     }
 }
