603bf95fe6d661cf9cd646a0989663becc73992b
tpenaruiz <tpenaruiz@gmail.com>
2017-02-16 23:21:38 +0100

    Updates

--- database/migrations/2014_10_20_000000_create_commandes_table.php
+++ database/migrations/2014_10_20_000000_create_commandes_table.php
@@ -17,7 +17,6 @@ class CreateCommandesTable extends Migration
             $table->increments('id')->unsigned();
             $table->integer('id_user')->unsigned();
             $table->integer('id_tva')->unsigned()->nullable();
-            $table->integer('id_commande_produit_pivot')->unsigned()->nullable();
             $table->integer('reference');
             $table->float('montant');
             $table->enum('status', array('En cours', 'Terminer', 'Annuler', 'Erreur'));
@@ -25,7 +24,6 @@ class CreateCommandesTable extends Migration
 
             $table->foreign('id_user')->references('id')->on('users');
             $table->foreign('id_tva')->references('id')->on('tva');
-            $table->foreign('id_commande_produit_pivot')->references('id')->on('commandes_produits_pivot');
         });
     }
 
