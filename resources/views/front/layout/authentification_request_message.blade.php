34f1628fb1da79197710809bef49105a9563a2e3
tpenaruiz <tpenaruiz@gmail.com>
2017-03-26 22:11:09 +0200

    Divers

--- resources/views/front/layout/authentification_request_message.blade.php
+++ resources/views/front/layout/authentification_request_message.blade.php
@@ -3,6 +3,7 @@
 <script type="text/javascript">
     if("{{ session('status') }}" != 0){ notie.alert(1, '{{ session('status') }}');}
     if("{{ session('warning') }}" != 0){ notie.alert(2, '{{ session('warning') }}');}
+    if("{{ session('danger') }}" != 0){ notie.alert(3, '{{ session('danger') }}');}
     /* Bloc d'authentification disparait à la validation du formulaire à cause du traitement en PHP
     Si erreur d'authentification redirection sur la page d'accueil avec message d'erreurs non visible
     Utilisation du plugin de notification pour signaler erreur de connexion */
